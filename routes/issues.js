631f7645e172da62c9072b5fa1445b97ae36f8a5
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-09-02 20:33:18 +0530

    issue list sorted

--- routes/issues.js
+++ routes/issues.js
@@ -2,15 +2,22 @@ var express = require('express');
 var router = express.Router();
 var issueModel = require('../models/issueModel');
 var helpers = require('../helpers');
-router.get('/', function(req, res , next) {
+router.get('/', function (req, res, next) {
     var getIssues = []
-      issueModel.find({}, function(err, issues) {
-        for (i in issues)
-            {console.log(i)
-              getIssues.push(issues[i]);
-            }
-              res.send(getIssues); 
-       });
-    
-  });
+    // issueModel.find({}, function (err, issues) {
+    //     for (i in issues) {
+    //         console.log(i)
+    //         getIssues.push(issues[i]);
+    //     }
+    //     res.send(getIssues);
+    // });
+    issueModel.find().sort({ upvotes: -1 }).exec(function (err, issues) {
+        for (i in issues) {
+            console.log(i)
+            getIssues.push(issues[i]);
+        }
+        res.send(getIssues);
+    });
+
+});
 module.exports = router;
