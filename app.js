2ad2cdcc38aa305fe6a608a3222a6f206d639dea
Rishav Agarwal <rishav.159@gmail.com>
2017-09-02 15:11:29 +0530

    Added ejs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +40,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{err:{
+    status : err.status,
+    msg : err.message
+  }});
 });
 
 module.exports = app;
