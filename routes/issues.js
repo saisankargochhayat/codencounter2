587988e63aba9ef4026b91389baad1a5102fcf24
Rishav Agarwal <rishav.159@gmail.com>
2017-09-02 20:28:50 +0530

    Adds front end for issues and dashboard

--- routes/issues.js
+++ routes/issues.js
@@ -3,4 +3,11 @@ var router = express.Router();
 var UserModel = require('../models/userModel');
 var helpers = require('../helpers');
 
+router.get('/',function(req,res,next) {
+  res.render('issues');
+});
+
+router.get('/getissue/:issueid',function(req,res,next) {
+  res.render('issue');
+});
 module.exports = router;
