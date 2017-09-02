ad3a7d3f5671c6d6f2c3fbeb73dd0aab90563378
Rishav Agarwal <rishav.159@gmail.com>
2017-09-02 15:21:16 +0530

    Added session package

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//Use session
+app.use(session({
+  secret: 'thenorthremembers',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
 
 // database setup
 var mongoose = require('mongoose');
