0ac19d60336df050f6b8a20f9c38ac9203610c5e
sohini-roy <sohinirc45@gmail.com>
2017-09-03 00:30:07 +0530

    lat-long added

--- obj_test_poll.js
+++ obj_test_poll.js
@@ -25,7 +25,7 @@ var obj = new Poll();
 obj.name = "Construction of over-bridge";
 obj.description = "BMC has been allocated funds to construct an over-bridge to ease traffic in the city";
 obj.city = "Bhubaneswar";
-obj.upvotes = 29;
+obj.upvotes = 110;
 obj.options = [{
     name: "Acharya Vihar to Nandankanan",
     votes: []
@@ -38,14 +38,36 @@ obj.options = [{
     }]
   }
 ];
-// obj.save(function(err){
-//   if ( err ) throw err;
-//   console.log("Object Saved Successfully");
-// });
 
 for (var i =0;i<50;i++){
   var latitute = (Math.random() * (20.295506 - 20.283511 + 1) + 20.283511).toFixed(6);
-  var longitude = (Math.random() * (85.842791 - 85.856266 + 1) + 85.856266).toFixed(6);  
+  var longitude = (Math.random() * (85.842791 - 85.856266 + 1) + 85.856266).toFixed(6);
+  obj.options[1].votes.push({
+    lat: String(latitute),
+    long: String(longitude)
+  });
+}
+
+for (var i =0;i<30;i++){
+  var latitute = (Math.random() * (20.295023 - 20.286409 + 1) + 20.286409).toFixed(6);
+  var longitude = (Math.random() * (85.825024 - 85.834379 + 1) + 85.834379).toFixed(6);
+  obj.options[1].votes.push({
+    lat: String(latitute),
+    long: String(longitude)
+  });
+}
+
+for (var i =0;i<20;i++){
+  var latitute = (Math.random() * (20.313537 - 20.302429 + 1) + 20.302429).toFixed(6);
+  var longitude = (Math.random() * (85.845280 - 85.827684 + 1) + 85.827684).toFixed(6);
+  obj.options[1].votes.push({
+    lat: String(latitute),
+    long: String(longitude)
+  });
+}
+for (var i =0;i<10;i++){
+  var latitute = (Math.random() * (20.298404 - 20.299727 + 1) + 20.299727).toFixed(6);
+  var longitude = (Math.random() * (85.804939 - 85.817264 + 1) + 85.817264).toFixed(6);
   obj.options[1].votes.push({
     lat: String(latitute),
     long: String(longitude)
