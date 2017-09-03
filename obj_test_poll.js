ba9b547d5d5f90b34c733f208c7bab569ed96be4
sohini-roy <sohinirc45@gmail.com>
2017-09-03 05:47:02 +0530

    minor fix

--- obj_test_poll.js
+++ obj_test_poll.js
@@ -69,7 +69,7 @@ obj3.options = [{
 for (var i =0;i<50;i++){
   var latitute = (Math.random() * (20.295506 - 20.283511) + 20.283511).toFixed(6);
   var longitude = (Math.random() * (85.842791 - 85.856266) + 85.856266).toFixed(6);
-  obj.options[1].votes.push({
+  obj1.options[1].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
@@ -78,16 +78,16 @@ for (var i =0;i<50;i++){
 for (var i =0;i<30;i++){
   var latitute = (Math.random() * (20.295023 - 20.286409) + 20.286409).toFixed(6);
   var longitude = (Math.random() * (85.834379 - 85.825024) + 85.825024).toFixed(6);
-  obj.options[1].votes.push({
+  obj1.options[1].votes.push({
     lat: String(latitute),
     long: String(longitude)
-  }
   });
+}
 
 for (var i =0;i<20;i++){
   var latitute = (Math.random() * (20.313537 - 20.302429) + 20.302429).toFixed(6);
   var longitude = (Math.random() * (85.845280 - 85.827684) + 85.827684).toFixed(6);
-  obj.options[1].votes.push({
+  obj1.options[1].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
@@ -96,7 +96,7 @@ for (var i =0;i<20;i++){
 for (var i =0;i<10;i++){
   var latitute = (Math.random() * (20.299727 - 20.298404) + 20.298404).toFixed(6);
   var longitude = (Math.random() * (85.817264 - 85.804939) + 85.804939).toFixed(6);
-  obj.options[1].votes.push({
+  obj1.options[1].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
@@ -105,7 +105,7 @@ for (var i =0;i<10;i++){
 for (var i =0;i<60;i++){
   var latitute = (Math.random() * (20.264849 - 20.263782) + 20.263782).toFixed(6);
   var longitude = (Math.random() * (85.818136 - 85.814445) + 85.814445).toFixed(6);
-  obj.options[0].votes.push({
+  obj1.options[0].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
@@ -114,7 +114,7 @@ for (var i =0;i<60;i++){
 for (var i =0;i<30;i++){
   var latitute = (Math.random() * (20.268311 - 20.265956) + 20.265956).toFixed(6);
   var longitude = (Math.random() * (85.816784 - 85.812922) + 85.812922).toFixed(6);
-  obj.options[0].votes.push({
+  obj1.options[0].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
@@ -123,7 +123,7 @@ for (var i =0;i<30;i++){
 for (var i =0;i<19;i++){
   var latitute = (Math.random() * (20.266315 - 20.265493) + 20.265493).toFixed(6);
   var longitude = (Math.random() * (85.810604 - 85.803266) + 85.803266).toFixed(6);
-  obj.options[0].votes.push({
+  obj1.options[0].votes.push({
     lat: String(latitute),
     long: String(longitude)
   });
